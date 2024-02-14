286d06ce4f8fc13acf9f38badd48e470e7fe0424
Technion <technion@lolware.net>
2024-02-14 17:09:37 +1100

    Fixed refreshes. Added lints.

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 import "./index.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // default: true
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
