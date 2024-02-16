50693b930cc08f0ddf99c8c2ea06b5e0f718985c
Technion <technion@lolware.net>
2024-02-16 14:08:42 +1100

    Base styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import {
   useQuery
 } from '@tanstack/react-query';
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 type FormValues = {
   domainname: string
 }
@@ -28,21 +24,16 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Office 365 Domain Enumerator</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text"  {...register("domainname", { pattern: /^[0-9a-z-.]{5,64}$/ })} />
-      <input type="submit" value="Post" />
+    <div className="flex flex-col items-center">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+      <label htmlFor="domain" className="block text-sm font-medium text-gray-700">Domain to Lookup</label>
+      <input type="text" id="domain"  {...register("domainname", { pattern: /^[0-9a-z-.]{5,64}$/ })} placeholder='lolware.net'
+                        className="w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]" />
+      <input type="submit" value="Post" className="text-white bg-[#007bff] hover:bg-blue-600 font-semibold rounded-md text-sm px-4 py-2.5 w-full" />
       </form>
-      {errors.domainname && <p role="alert"> BIG ERROR</p>}
+      {errors.domainname && <InputInvalid /> }
       {domainName && <EnumerateOutput lookup={JSON.stringify(domainName)}></EnumerateOutput>}
+      </div>
     </>
   )
 }
@@ -64,15 +55,32 @@ const EnumerateOutput = ({lookup}: {lookup: string}) => {
   });
 
   if(error) {
-    return (<>BIG SWEATY FETCH ERROR {error}</>)
+    return (
+      <div className="my-10 relative block rounded-lg bg-pink-500 p-4 text-base leading-5 text-white">
+      Backend lookup failed. This may mean the domain is not registered with Microsoft, or that the query was considered invalid.
+    </div>
+    )
   }
 
   if(isFetching) {
-    return (<>CURRENTLY FETCHING</>)
+    return (<div className="my-10 relative block rounded-lg bg-green-200 p-4 text-base leading-5 text-white">
+      Querying Backend...
+      </div>)
   }
 
   return (
     <>{JSON.stringify(data)}</>
+  )
+}
+
+const InputInvalid = () => {
+  return (
+    <>
+    <div className="my-10 relative block rounded-lg bg-pink-500 p-4 text-base leading-5 text-white">
+      Invalid Input - Query Rejected
+    </div>
+    <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZWJkMWJnaTJ1b2Q0NDM0MHpmdWllbmlweDJzdTR3YXMxcDR0b3lrNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MM0Jrc8BHKx3y/giphy.gif" />
+    </>
   )
 }
  
