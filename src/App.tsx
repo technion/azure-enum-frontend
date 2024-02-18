d5324f28f152e5156646be03ab28971cc1545195
Technion <technion@lolware.net>
2024-02-18 16:51:18 +1100

    Give up on Trusted Types experiement

--- src/App.tsx
+++ src/App.tsx
@@ -1,110 +1,119 @@
-import { useState } from 'react'
-import { useForm, SubmitHandler } from "react-hook-form"
-import {
-  useQuery
-} from '@tanstack/react-query';
-import hackingInProgress from './assets/hacking in progress.gif';
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useQuery } from "@tanstack/react-query";
+import hackingInProgress from "./assets/hacking in progress.gif";
 
 type FormValues = {
-  domainname: string
-}
-
+  domainname: string;
+};
 
 function App() {
-  const [domainName, setDomainName] = useState<undefined | FormValues>(undefined);
+  const [domainName, setDomainName] = useState<undefined | FormValues>(
+    undefined
+  );
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<FormValues>();
 
-
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     setDomainName(data);
-  }
+  };
 
   return (
     <>
-    <div className="flex flex-col items-center">
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-      <label htmlFor="domain" className="block text-sm font-medium text-gray-700">Domain to Lookup</label>
-      <input type="text" id="domain"  {...register("domainname", { pattern: /^[0-9a-z-.]{5,64}$/, required: true })} placeholder='lolware.net'
-                        className="w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]" />
-      <input type="submit" value="Post" className="text-white bg-[#007bff] hover:bg-blue-600 font-semibold rounded-md text-sm px-4 py-2.5 w-full" />
-      </form>
-      {errors.domainname && <InputInvalid /> }
-      {domainName && <EnumerateOutput lookup={JSON.stringify(domainName)}></EnumerateOutput>}
+      <div className="flex flex-col items-center">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <label
+            htmlFor="domain"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Domain to Lookup
+          </label>
+          <input
+            type="text"
+            id="domain"
+            {...register("domainname", {
+              pattern: /^[0-9a-z-.]{5,64}$/,
+              required: true,
+            })}
+            placeholder="lolware.net"
+            className="w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]"
+          />
+          <input
+            type="submit"
+            value="Post"
+            className="text-white bg-[#007bff] hover:bg-blue-600 font-semibold rounded-md text-sm px-4 py-2.5 w-full"
+          />
+        </form>
+        {errors.domainname && <InputInvalid />}
+        {domainName && (
+          <EnumerateOutput
+            lookup={JSON.stringify(domainName)}
+          ></EnumerateOutput>
+        )}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
 
-const EnumerateOutput = ({lookup}: {lookup: string}) => {
-
+const EnumerateOutput = ({ lookup }: { lookup: string }) => {
   const { data, error, isFetching } = useQuery({
-    queryKey: ['domainName', lookup],
+    queryKey: ["domainName", lookup],
     queryFn: async () => {
-      const response = await fetch('https://azure-enum-api.lolware.net/', {
+      const response = await fetch("https://azure-enum-api.lolware.net/", {
         method: "POST",
-        headers: { "Content-Type": "application/json"},
-        body: lookup
+        headers: { "Content-Type": "application/json" },
+        body: lookup,
       });
       if (!response.ok) {
-        throw new Error('Network response was not ok')
+        throw new Error("Network response was not ok");
       }
       return await response.json();
-    }
+    },
   });
 
-  if(error) {
+  if (error) {
     return (
       <div className="my-10 relative block rounded-lg bg-pink-500 p-4 text-base leading-5 text-white">
-      Backend lookup failed. This may mean the domain is not registered with Microsoft, or that the query was considered invalid.
-    </div>
-    )
+        Backend lookup failed. This may mean the domain is not registered with
+        Microsoft, or that the query was considered invalid.
+      </div>
+    );
   }
 
-  if(isFetching) {
-    return (<div className="my-10 relative block rounded-lg bg-green-200 p-4 text-base leading-5 text-white">
-      Querying Backend...
-      </div>)
+  if (isFetching) {
+    return (
+      <div className="my-10 relative block rounded-lg bg-green-200 p-4 text-base leading-5 text-white">
+        Querying Backend...
+      </div>
+    );
   }
 
-  return (
-    <DomainList data={data.output} />
-  )
-}
+  return <DomainList data={data.output} />;
+};
 
 const InputInvalid = () => {
   return (
     <>
-    <div className="my-10 relative block rounded-lg bg-pink-500 p-4 text-base leading-5 text-white">
-      Invalid Input - Query Rejected
-    </div>
-    <img src={hackingInProgress} alt="Hacking in progress" />
+      <div className="my-10 relative block rounded-lg bg-pink-500 p-4 text-base leading-5 text-white">
+        Invalid Input - Query Rejected
+      </div>
+      <img src={hackingInProgress} alt="Hacking in progress" />
     </>
-  )
-}
-
-//import { trustedTypes } from 'trusted-types';
+  );
+};
 
 const DomainList = ({ data }: { data: string }) => {
-  // Technically untrusted data being parsed this way requires a Trusted Types policy.
-  
-  const safeXMLPolicy = trustedTypes.createPolicy("myEscapePolicy", {
-    createHTML: (string: string) => {
-      const d = new DOMParser();
-      // This is considered an untrusted XML parsing, but the document constructed is never directly rendered or
-      const r = d.parseFromString(string, "application/xml");
-      const domainlist = r.documentElement.querySelectorAll("Domain");
-      // SAFETY: This whole function ends up using textContent() which is safe
-      return [...domainlist].map((e) => e.textContent).join(" ");
-    },
-  });
+  // This function is ultimately incompatible with Trusted Types, resulting in the feature's removal
 
-  const listmap: TrustedHTML = safeXMLPolicy.createHTML(data);
+  const d = new DOMParser();
+  const r = d.parseFromString(data, "application/xml");
+  const domainlist = r.documentElement.querySelectorAll("Domain");
+  const listmap = [...domainlist].map((e) => e.textContent).join(" ");
 
   return <>{listmap}</>;
 };
